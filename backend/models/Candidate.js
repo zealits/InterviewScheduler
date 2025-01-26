2accc1705034cec49fe97ff51c54cd90c0b3f1af
Rahul Gawade <rahulgawade360@gmail.com>
2025-01-26 21:54:16 +0530

    update for slot booking

---
+++ backend/models/Candidate.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const candidateSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  linkedin: {
+    type: String,
+    required: true
+  },
+  jobDescription: {
+    type: String,
+    required: true
+  }
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+
+module.exports = Candidate;
